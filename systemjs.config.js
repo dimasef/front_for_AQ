a5fae34aa392867540126966830e2b656e93d861
IliaVolk <liailia119@gmail.com>
2016-11-01 03:55:10 +0200

    forgoten files

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,6 +26,7 @@
       // other libraries
       'rxjs':                       'npm:rxjs',
       'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+      'primeng':                    'node_modules/primeng'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -39,7 +40,8 @@
       'angular2-in-memory-web-api': {
         main: './index.js',
         defaultExtension: 'js'
-      }
+      },
+      'primeng': { defaultExtension: 'js' }
     }
   });
 })(this);
